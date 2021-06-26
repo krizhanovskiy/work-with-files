b6c35a53fbe0b48216213a57766c508a4d150ad8
Vladimir <713641@gmail.com>
2021-06-26 14:46:30 +0300

    add test for class DocFileTests

--- src/test/java/tests/DocFileTests.java
+++ src/test/java/tests/DocFileTests.java
@@ -15,4 +15,13 @@ public class DocFileTests {
         String actualData = readDocFromPath(docFilePath);
         assertThat(actualData, containsString(expectedData));
     }
+
+    @Test
+    void docTest1() {
+        String docFilePath = "./src/test/resources/files/work_with_file_in_java.doc";
+        String expectedData = "test work with doc file";
+
+        String actualData = readDocFromPath(docFilePath);
+        assertThat(actualData, containsString(expectedData));
+    }
 }
